92bb66f013a9f408d26ea4c485ac662d2d4e115b
Henry <cryptix@riseup.net>
2024-06-20 14:14:49 +0200

    cleanup ecrecover code

--- ethereum_verify.go
+++ ethereum_verify.go
@@ -13,8 +13,12 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-func (c ethClient) verifyChallengeResponse(publicKey, challange, signature []byte) error {
-	sighash := accounts.TextHash(challange)
+func ecrecoverEIP191(message, signature []byte) ([]byte, error) {
+	if len(signature) != 65 {
+		return nil, fmt.Errorf("signature length is not 65")
+	}
+
+	sighash := accounts.TextHash(message)
 
 	// update the recovery id
 	// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L442
@@ -23,8 +27,22 @@ func (c ethClient) verifyChallengeResponse(publicKey, challange, signature []byt
 	// get the pubkey used to sign this signature
 	recovered, err := crypto.Ecrecover(sighash, signature)
 	if err != nil {
-		return fmt.Errorf("ecrecover: %w", err)
+		return nil, fmt.Errorf("ecrecover: %w", err)
 	}
+
+	return recovered, nil
+}
+
+func ecrecoverEIP191AndCompare(message, signature, publicKey []byte) error {
+	if len(publicKey) != 64 {
+		return fmt.Errorf("publicKey length is not 64")
+	}
+
+	recovered, err := ecrecoverEIP191(message, signature)
+	if err != nil {
+		return err
+	}
+
 	if len(recovered) == 65 && recovered[0] == 0x04 {
 		// split of encoding bit
 		recovered = recovered[1:]
@@ -33,65 +51,30 @@ func (c ethClient) verifyChallengeResponse(publicKey, challange, signature []byt
 	if subtle.ConstantTimeCompare(recovered, publicKey) != 1 {
 		return fmt.Errorf("keys are not equal")
 	}
-
 	return nil
 }
 
+func (c ethClient) verifyChallengeResponse(publicKey, challange, signature []byte) error {
+	return ecrecoverEIP191AndCompare(challange, signature, publicKey)
+}
+
 func (c ethClient) verifyKeyCardEnroll(keyCardPublicKey, signature []byte) (common.Address, error) {
-	if len(signature) != 65 {
-		return common.Address{}, fmt.Errorf("signature length is not 65")
-	}
-
-	if len(keyCardPublicKey) != 64 {
-		return common.Address{}, fmt.Errorf("keyCardPublicKey length is not 64")
-	}
-
-	sighash := accounts.TextHash(keyCardPublicKey)
-
-	// update the recovery id
-	// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L442
-	signature[64] -= 27
-
-	// get the pubkey used to sign this signature
-	recovered, err := crypto.Ecrecover(sighash, signature)
-	if err != nil {
-		return common.Address{}, fmt.Errorf("ecrecover: %w", err)
-	}
-	pubkey, err := crypto.UnmarshalPubkey(recovered)
+	rec, err := ecrecoverEIP191(keyCardPublicKey, signature)
 	if err != nil {
 		return common.Address{}, fmt.Errorf("UnmarshalPubkey failed: %w", err)
 	}
-	recoveredAddress := crypto.PubkeyToAddress(*pubkey)
 
+	pubkey, err := crypto.UnmarshalPubkey(rec)
+	if err != nil {
+		return common.Address{}, fmt.Errorf("UnmarshalPubkey failed: %w", err)
+	}
+
+	recoveredAddress := crypto.PubkeyToAddress(*pubkey)
 	return recoveredAddress, nil
 }
 
 func (c ethClient) eventVerify(evt *SignedEvent, publicKey []byte) error {
-	assert(evt != nil)
-	sighash := accounts.TextHash(evt.Event.Value)
-
-	signature := evt.Signature
-
-	// update the recovery id
-	// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L442
-	signature[64] -= 27
-
-	// get the pubkey used to sign this signature
-	recovered, err := crypto.Ecrecover(sighash, signature)
-	if err != nil {
-		return fmt.Errorf("verifyEvent: ecrecover failed: %w", err)
-	}
-
-	if len(recovered) == 65 && recovered[0] == 0x04 {
-		// split of encoding bit
-		recovered = recovered[1:]
-	}
-
-	if subtle.ConstantTimeCompare(recovered, publicKey) != 1 {
-		return fmt.Errorf("verifyEvent: keys are not equal")
-	}
-
-	return nil
+	return ecrecoverEIP191AndCompare(evt.Event.Value, evt.Signature, publicKey)
 }
 
 func (c ethClient) eventSign(evtData []byte) ([]byte, error) {
