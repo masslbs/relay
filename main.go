9101ef00b01cf476cc43d043d94d3f7b6295ac9e
Henry <henry@mass.market>
2024-09-19 11:34:22 +0200

    move combinedID to ids.go

--- main.go
+++ main.go
@@ -1677,59 +1677,6 @@ type CachedStock struct {
 	inventory *MapInts[combinedID, int32]
 }
 
-type combinedID struct {
-	listingID uint64
-
-	// uint64 ids delimited by :
-	// side-stepping the problem that you can't have a slice in a comparable struct
-	variations string
-}
-
-func (cid combinedID) Hash() common.Hash {
-	var buf [8]byte
-	hasher := sha3.NewLegacyKeccak256()
-	binary.BigEndian.PutUint64(buf[:], cid.listingID)
-	hasher.Write(buf[:])
-
-	if cid.variations != "" {
-		varStrs := strings.Split(cid.variations, ":")
-		for _, vidStr := range varStrs {
-			vid, err := strconv.ParseUint(vidStr, 10, 64)
-			check(err)
-			binary.BigEndian.PutUint64(buf[:], vid)
-			hasher.Write(buf[:])
-		}
-	}
-	return common.Hash(hasher.Sum(nil))
-}
-
-func newCombinedID(listingID uint64, variations ...uint64) combinedID {
-	slices.Sort(variations)
-	varStr := make([]string, len(variations))
-	for i, v := range variations {
-		varStr[i] = strconv.FormatUint(v, 10)
-	}
-	return combinedID{
-		listingID:  listingID,
-		variations: strings.Join(varStr, ":"),
-	}
-}
-
-func (cid combinedID) Variations() []uint64 {
-	if cid.variations == "" {
-		return nil
-	}
-	varStrs := strings.Split(cid.variations, ":")
-	vids := make([]uint64, len(varStrs))
-	var err error
-	for i, vidstr := range varStrs {
-		vids[i], err = strconv.ParseUint(vidstr, 10, 64)
-		check(err)
-	}
-
-	return vids
-}
-
 func (current *CachedStock) update(evt *ShopEvent, _ CachedMetadata) {
 	cs := evt.GetChangeInventory()
 	if cs == nil {
